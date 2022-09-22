dd0e0ce895a3e250b31c7eff9c1741f80e8dd94d
Mathieu Ford <mathieu.ford@gmail.com>
2022-09-22 14:39:49 -0400

    Added cannons to test scene

--- Cannon/Scripts/Cannon.gd
+++ Cannon/Scripts/Cannon.gd
@@ -5,17 +5,22 @@ onready var _cannon_ball : PackedScene = preload("res://CannonBall/CannonBall.ts
 onready var _projectile_spawn : Position2D = $ProjectileSpawn
 onready var _animated_sprite : AnimatedSprite = $AnimatedSprite
 
+export var is_starboard : bool = true
+
+var _shoot_key : String
+
 func _ready() -> void:
+	_shoot_key = "right_arrow" if is_starboard else "left_arrow"
 	_animated_sprite.connect("animation_finished", self, "_animation_finished")
 
-func _unhandled_input(event) -> void:
-	if (event.is_action_pressed("right_arrow")):
+func _unhandled_input(event : InputEvent) -> void:
+	if (event.is_action_pressed(_shoot_key)):
 		_animated_sprite.play("ChargeUp")
 	
-	elif (event.is_action_released("right_arrow") and not _animated_sprite.playing):
+	elif (event.is_action_released(_shoot_key) and not _animated_sprite.playing):
 		_animated_sprite.play("Fire")
 	
-	elif (_animated_sprite.playing):
+	elif (event.is_action_released(_shoot_key) and _animated_sprite.playing):
 		_animated_sprite.play("Idle")
 
 
@@ -23,7 +28,7 @@ func _animation_finished() -> void:
 	if (_animated_sprite.animation == "Fire"):
 		var instance = _cannon_ball.instance()
 		instance.position = _projectile_spawn.global_position
-		instance.direction = Vector2(cos(rotation), sin(rotation))
+		instance.direction = Vector2(cos(global_rotation), sin(global_rotation))
 		add_child(instance)
 	
 	elif (_animated_sprite.animation == "ChargeUp"):
