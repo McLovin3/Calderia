ae5112e2a104475e8f614b20b5c0a817d3128f49
Mathieu Ford <mathieu.ford@gmail.com>
2022-10-11 10:20:43 -0400

    Refactored

--- GameManager/Scripts/CustomizationMenu.gd
+++ GameManager/Scripts/CustomizationMenu.gd
@@ -15,11 +15,7 @@ onready var _dialog : ConfirmationDialog = $ConfirmationDialog
 onready var _alert_dialog : AcceptDialog = $AlertDialog
 
 var _inventory_open : bool = false
-var _tools : Dictionary
 var _last_selected_item : Dictionary
-var _last_selected_item_name : String
-var _is_right : bool
-var _last_selected_index : int
 
 func _ready() -> void:
 	_dialog.get_close_button().disabled = true
@@ -66,60 +62,46 @@ func _on_Tween_tween_all_completed():
 	if not _inventory_open:
 		visible = false
 
-func _on_LeftItem_item_selected(index: int) -> void:
-	_tools = GameManager.get_tools()
+func _select_item(is_right: bool, index: int) -> void:
+	var tools = GameManager.get_tools()
+	_last_selected_item.is_right = is_right
+	_last_selected_item.index = index
+	
 	if player:
 		if index == 0:
-			player.clear_left_tool()
+			player.clear_right_tool() if is_right else player.clear_left_tool()
 		elif index == 1:
-			if not _tools.leftTools.cannon.unlocked:
+			if not tools.cannon.unlocked:
 				_left_button.selected = 0
 				_dialog.dialog_text = "Acheter pour %s bois et %s pierres?" \
-					% [_tools.leftTools.cannon.price.wood, _tools.leftTools.cannon.price.stone]
-				_last_selected_item = _tools.leftTools.cannon
-				_last_selected_item_name = "cannon"
-				_is_right = false
-				_last_selected_index = 1
+					% [tools.cannon.wood, tools.cannon.stone]
+				_last_selected_item.wood = tools.cannon.wood
+				_last_selected_item.stone = tools.cannon.stone
+				_last_selected_item.name = "cannon"
 				_dialog.popup()
 			else:
-				player.set_left_tool(_cannon)
+				player.set_right_tool(_cannon) if is_right else player.set_left_tool(_cannon)
+
+func _on_LeftItem_item_selected(index: int) -> void:
+	_select_item(false, index)
 
 func _on_RightItem_item_selected(index: int) -> void:
-	_tools = GameManager.get_tools()
-	if player:
-		if index == 0:
-			player.clear_right_tool()
-		elif index == 1:
-			if not _tools.rightTools.cannon.unlocked:
-				_right_button.selected = 0
-				_dialog.dialog_text = "Acheter pour %s bois et %s pierres?" \
-					% [_tools.rightTools.cannon.price.wood, _tools.rightTools.cannon.price.stone]
-				_last_selected_item = _tools.rightTools.cannon
-				_last_selected_item_name = "cannon"
-				_last_selected_index = 1
-				_is_right = true
-				_dialog.popup()
-			else:
-				player.set_right_tool(_cannon)
+	_select_item(true, index)
 
 func _on_ConfirmationDialog_confirmed()-> void:
 	var ressources = GameManager.get_ressources()
 	
-	if (_last_selected_item.price.wood > ressources.wood
-		or _last_selected_item.price.stone > ressources.stone):
+	if (_last_selected_item.wood > ressources.wood
+		or _last_selected_item.stone > ressources.stone):
 		_alert_dialog.popup()
 		return
 	
-	ressources.stone -= _last_selected_item.price.stone
-	ressources.wood -= _last_selected_item.price.wood
-	GameManager.set_ressources(ressources)
-	GameManager.unlock_item(_is_right, _last_selected_item_name)
-	_tools = GameManager.get_tools()
+	GameManager.unlock_item(_last_selected_item.name)
 	
-	if _is_right:
-		_on_RightItem_item_selected(_last_selected_index)
-		_right_button.selected = _last_selected_index
+	if _last_selected_item.is_right:
+		_on_RightItem_item_selected(_last_selected_item.index)
+		_right_button.selected = _last_selected_item.index
 	
 	else:
-		_on_LeftItem_item_selected(_last_selected_index)
-		_left_button.selected = _last_selected_index
+		_on_LeftItem_item_selected(_last_selected_item.index)
+		_left_button.selected = _last_selected_item.index
--- GameManager/Scripts/GameManager.gd
+++ GameManager/Scripts/GameManager.gd
@@ -7,37 +7,20 @@ onready var _health_bar : ProgressBar = $HUD/HealthBar
 onready var _dash_bar : ProgressBar = $HUD/DashBar
 
 var _save_path : String = "user://savegame.save"
-var data : Dictionary = \
+var _data : Dictionary = \
 	{
 		"ressources" : 
 			{
 				"wood" : 0, 
-				"stone" : 0}, 
+				"stone" : 0
+			}, 
 		"tools": 
 			{
-				"leftTools" : 
+				"cannon" : 
 					{
-						"cannon" : 
-							{
-								"unlocked" : false,
-								"price" : 
-									{
-										"wood" : 100,
-										"stone" : 100
-									}
-							}
-					},
-				"rightTools" : 
-					{
-						"cannon" : 
-							{
-								"unlocked" : false,
-								"price" : 
-									{
-										"wood" : 100,
-										"stone" : 100
-									}
-							}
+						"unlocked" : false,
+						"wood" : 100,
+						"stone" : 100
 					}
 			}
 	} 
@@ -51,7 +34,7 @@ func _load_game() -> void:
 		_save_game()
 	
 	var _error = save_file.open_encrypted_with_pass(_save_path, File.READ, "McLovin")
-	data = parse_json(save_file.get_line())
+	_data = parse_json(save_file.get_line())
 	
 	_update_labels()
 	
@@ -60,18 +43,18 @@ func _load_game() -> void:
 func _save_game() -> void:
 	var save_file := File.new()
 	var _error = save_file.open_encrypted_with_pass(_save_path, File.WRITE, "McLovin")
-	save_file.store_line(to_json(data))
+	save_file.store_line(to_json(_data))
 	save_file.close()
 
 func _update_labels() -> void:
-	_stone_label.text = String(data.ressources.stone)
-	_wood_label.text = String(data.ressources.wood)
+	_stone_label.text = String(_data.ressources.stone)
+	_wood_label.text = String(_data.ressources.wood)
 
 func _on_SaveInterval_timeout() -> void:
 	_save_game()
 
 func add_wood(amount: int) -> void:
-	data.ressources.wood += amount
+	_data.ressources.wood += amount
 	_update_labels()
 	_save_game()
 
@@ -82,7 +65,7 @@ func set_dash(current_dash : float, max_dash : float) -> void:
 	_dash_bar.value = (current_dash * _dash_bar.max_value) / max_dash
 
 func add_stone(amount: int) -> void:
-	data.ressources.stone += amount
+	_data.ressources.stone += amount
 	_update_labels()
 	_save_game()
 
@@ -90,21 +73,22 @@ func set_player(player: PlayerShip) -> void:
 	_customization_menu.player = player
 
 func get_tools() -> Dictionary:
-	return data.tools
+	return _data.tools
 
 func get_ressources() -> Dictionary:
-	return data.ressources
+	return _data.ressources
 
 func set_ressources(ressources : Dictionary) -> void:
-	data.ressources = ressources
-	_save_game()
+	_data.ressources = ressources
 	_update_labels()
+	_save_game()
 
-func unlock_item(isRightSide : bool, tool_name : String) -> void:
-	if isRightSide:
-		data.tools.rightTools.get(tool_name).unlocked = true
+func unlock_item(tool_name : String) -> void:
+	var selected_tool : Dictionary = _data.tools.get(tool_name)
 	
-	else:
-		data.tools.leftTools.get(tool_name).unlocked = true
+	selected_tool.unlocked = true
+	_data.ressources.wood -= selected_tool.wood
+	_data.ressources.stone -= selected_tool.stone
 	
+	_update_labels()
 	_save_game()
